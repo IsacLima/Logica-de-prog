c91c2b1fb066c66d4242e9a000810570e816d0c6
IsacLima <isaque.lima.leite.ill@gmail.com>
2024-09-12 12:02:54 -0300

    add TwentyPercent.java code

--- Lista01/ex2/TwentyPercent.java
+++ Lista01/ex2/TwentyPercent.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class TwentyPercent{
+
+    public static void main(String[] args){
+        float number;
+
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Digite um valor a ser adicionado 20%: ");
+        number = read.nextFloat();
+
+        System.out.println("o valor de " + number + " adicionado 20% fica: " + (number + (number*0.2)));
+
+        read.close();
+
+    }
+}
