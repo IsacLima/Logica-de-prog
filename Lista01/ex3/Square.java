76e72757d1e7292153dff50f3b110ba1b186018c
Isaque Lima Leite <30935061+IsacLima@users.noreply.github.com>
2024-09-12 15:55:57 -0300

    Update Square.java

--- Lista01/ex3/Square.java
+++ Lista01/ex3/Square.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+
+public class Square{
+    public static void main(String[] args){
+        Scanner read = new Scanner(System.in);
+        int number;
+
+        System.out.println("Digite o número que será elevado ao quadrado: ");
+        number = read.nextInt();
+        System.out.println("O quadrado de " + number + " é : " + (number*number));
+
+        read.close();
+    }
+}
 
