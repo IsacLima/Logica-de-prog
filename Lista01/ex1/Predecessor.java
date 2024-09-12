02f7a7b1a173d19f0d67f999120c3f71138e8fa5
Isaque Lima Leite <30935061+IsacLima@users.noreply.github.com>
2024-09-12 10:57:17 -0300

    Update Predecessor.java

--- Lista01/ex1/Predecessor.java
+++ Lista01/ex1/Predecessor.java
@@ -1,1 +1,23 @@
+//importo a função scanner que lê entradas
+import java.util.Scanner;
+
+
+public class Predecessor{
+   
+    public static void main(String[] args) {
+        //declaro as variaveis que usarei
+        int number;
+        
+        //instancio a função de leitura
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Informe um número para descobrir seu antecessor: ");
+        
+        //recebo o retorno da leitura (nextInt() = o valor lido será inteiro)
+        number = read.nextInt();
+
+        //printo a saída
+        System.out.println("O número antecessor ao " + number +  " é: " + (number-1));
+    }
+}
 
