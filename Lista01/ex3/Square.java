36146e447fd37e78c9f775145613f090af98c8d6
Isac Lima <isaque.lima.leite.ill@gmail.com>
2024-09-14 15:58:39 -0300

    add a new var to make the code cleaner

--- Lista01/ex3/Square.java
+++ Lista01/ex3/Square.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Square{
     public static void main(String[] args){
         Scanner read = new Scanner(System.in);
-        int number;
+        int number, square;
 
         System.out.println("Digite o número que será elevado ao quadrado: ");
+        
         number = read.nextInt();
-        System.out.println("O quadrado de " + number + " é : " + (number*number));
+        square = (number*number);
+
+        System.out.println("O quadrado de " + number + " é : " + square);
 
         read.close();
     }
