8f01b56deb68fbbaee69a8299255659458e30d8f
Isac Lima <isaque.lima.leite.ill@gmail.com>
2024-09-14 16:06:18 -0300

    add a new var to make the code cleaner

--- Lista01/ex5/Celsius.java
+++ Lista01/ex5/Celsius.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class Celsius{
     public static void main(String[] args){
-        float temp;
+        float temp, celsius;
+
         Scanner read = new Scanner(System.in);
+        
         System.out.println("Digite o valor da temperatura em Fahrenheit: ");
 
         temp = read.nextFloat();
-        
-        System.out.println("A temperatura em Fahrenheit é " + temp + " e em Celsius é " + (((temp - 32)/9)*5));
+        celsius = (((temp - 32)/9)*5);
+
+        System.out.println("A temperatura em Fahrenheit é " + temp + " e em Celsius é " + celsius);
         
         read.close();
 
