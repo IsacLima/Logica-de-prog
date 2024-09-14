bac640c5eb654131cacbadfd98a53c0703645a06
Isac Lima <isaque.lima.leite.ill@gmail.com>
2024-09-14 16:01:15 -0300

    add spaces to make the code cleaner

--- Lista01/ex4/Triangle.java
+++ Lista01/ex4/Triangle.java
@@ -6,11 +6,15 @@ public class Triangle{
     public static void main(String[] args){
         Scanner read = new Scanner(System.in);
         int area, b, h;
+
         System.out.println("Qual a altura do triangulo? ");
         h = read.nextInt();
+        
         System.out.println("E qual o valor da base?");
         b = read.nextInt();
+        
         area = (b*h)/2;
+        
         System.out.println("A área desse triângulo é equivalente a " + area);
 
         read.close(); 
