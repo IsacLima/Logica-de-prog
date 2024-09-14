755d95befb6f93598ec286d21ce0664a7fe782de
Isac Lima <isaque.lima.leite.ill@gmail.com>
2024-09-14 16:11:02 -0300

    add a new var to make the code cleaner

--- Lista01/ex6/Media.java
+++ Lista01/ex6/Media.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 
  public class Media{
     public static void main(String[] args){
-        float grade1, grade2;
+        float grade1, grade2, media;
         Scanner read = new Scanner(System.in);
-        System.out.print("Digite a nota da primeira prova: ");
+
+        System.out.println("Digite as notas das provas: ");
+       
         grade1 = read.nextFloat();
-        System.out.print("Digite a nota da segunda prova: ");
         grade2 = read.nextFloat();
-        System.out.println("A média entre " + grade1 + " e " + grade2 + " é: " + ((grade1+grade2)/2));
+        
+        media = ((grade1+grade2)/2);
+
+        System.out.println("A média entre " + grade1 + " e " + grade2 + " é: " + media);
         read.close();
     }
  }
